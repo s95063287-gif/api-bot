196089700989badfa4ea05b768c71dcd1791fa88
s95063287 <s95063287@gmail.com>
2026-03-16 18:35:20 +0100

    fix: disable xForwardedFor validation in rateLimiter

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -3,7 +3,8 @@ const rateLimit = require('express-rate-limit');
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000,
     max: 100,
-    message: "Muitas requisições vindas deste IP, tente novamente mais tarde."
+    message: "Muitas requisições vindas deste IP, tente novamente mais tarde.",
+    validate: { xForwardedForHeader: false } // ← NEU
 });
 
 module.exports = limiter;
