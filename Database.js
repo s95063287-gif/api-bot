5a94c1a74f5a917c56385aaff4f306bdd03ee8de
s95063287 <s95063287@gmail.com>
2026-03-16 18:46:52 +0100

    fix: simplified mongoose connection

--- Database.js
+++ Database.js
@@ -2,25 +2,19 @@ require('colors');
 const mongoose = require('mongoose');
 require('dotenv').config({ path: "./.env" });
 
-let cached = global.mongoose;
-if (!cached) cached = global.mongoose = { conn: null, promise: null };
-
 async function connectToDatabase() {
-    if (cached.conn) return cached.conn;
-
-    if (!cached.promise) {
-        cached.promise = mongoose.connect(process.env.MONGODB_URL, {
-            serverSelectionTimeoutMS: 30000,
-            socketTimeoutMS: 45000,
-            bufferCommands: false,
-        }).then(mongoose => {
-            console.log("Sucesso ".green + 'Conectado ao MongoDB');
-            return mongoose;
-        });
-    }
-
-    cached.conn = await cached.promise;
-    return cached.conn;
+    if (mongoose.connection.readyState >= 1) return;
+    
+    return mongoose.connect(process.env.MONGODB_URL, {
+        serverSelectionTimeoutMS: 30000,
+        socketTimeoutMS: 45000,
+        bufferCommands: false,
+    }).then(() => {
+        console.log("Sucesso ".green + 'Conectado ao MongoDB');
+    }).catch(err => {
+        console.error("Falha ".red + 'Erro ao conectar:', err.message);
+        throw err;
+    });
 }
 
 module.exports = connectToDatabase;
