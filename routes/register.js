0d63f452676e80d3c7a0a97180e2a810a73ea47e
s95063287 <s95063287@gmail.com>
2026-03-16 20:08:47 +0100

    fix: register route path

--- routes/register.js
+++ routes/register.js
@@ -1,33 +1,32 @@
 const express = require('express');
 const router = express.Router();
-
 const User = require('../models/user');
-const Key = require('../models/key')
-
+const Key = require('../models/key');
 const rateLimiter = require('../middleware/rateLimiter');
-router.post('/register', rateLimiter, async (req, res) => {
-    console.log('Requisição recebida em /register:', req.body);
+const connectToDatabase = require('../Database');
+
+router.post('/', rateLimiter, async (req, res) => {
+    await connectToDatabase();
     const { hwid, username, key, password } = req.body;
 
-    // Verificar a validade da chave
-    let validKey = await Key.findOne({ key, used: false, expirationDate: { $gte: new Date() } });
-    if (!validKey) {
-        return res.status(403).json({ error: 'Chave inválida ou expirada.' });
+    try {
+        let validKey = await Key.findOne({ key, used: false, expirationDate: { $gte: new Date() } });
+        if (!validKey) {
+            return res.status(403).json({ error: 'Chave inválida ou expirada.' });
+        }
+
+        validKey.used = true;
+        await validKey.save();
+
+        let expirationDate = new Date(validKey.expirationDate);
+        const user = new User({ hwid, username, expirationDate, password });
+        await user.save();
+
+        res.json({ message: 'Login criado com sucesso', expirationDate });
+    } catch (error) {
+        console.error('Erro ao registrar:', error);
+        res.status(500).json({ error: 'Erro ao registrar.' });
     }
-
-    // Marcar a chave como usada
-    validKey.used = true;
-    await validKey.save();
-
-    let expirationDate = new Date(validKey.expirationDate);
-
-    const user = new User({ hwid, username, expirationDate, password });
-    console.log('Criando novo User:', user);
-
-    await user.save();
-    console.log('User salvo no banco de dados:', user);
-
-    res.json({ message: 'Login criado com sucesso', expirationDate });
 });
 
 module.exports = router;
